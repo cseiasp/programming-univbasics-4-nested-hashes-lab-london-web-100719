8b8fc4df190421e46d5cc18c1e6a5800b9ba6b24
Caroline S-A <caroline.seilern@hotmail.com>
2019-09-05 17:53:29 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,10 +81,10 @@ def adding_matz
           :known_for => "Unix",
           :languages => ["C"]
         }
-        :yukihiro_matsumoto => {
-          :known_for => "Ruby",
-          :languages => ["LISP", "C"]
-        }
+        #:yukihiro_matsumoto => {
+          #:known_for => "Ruby",
+          #:languages => ["LISP", "C"]
+        #}
  		}
 
    #programmer_hash[:key] = "value"
