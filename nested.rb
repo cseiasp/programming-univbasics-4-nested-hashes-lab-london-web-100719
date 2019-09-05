9cad663ff2603d71c8030128072f51f15efd6e7f
Caroline S-A <caroline.seilern@hotmail.com>
2019-09-05 17:50:23 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -60,14 +60,14 @@ def dennis_ritchies_language
      programmer_hash[:dennis_ritchie][:languages][0]
 end
 
-def adding_matz
+def adding_matz(key)
 # add the following information to the top level of programmer_hash
 # :yukihiro_matsumoto => {
 #   :known_for => "Ruby",
 #    :languages => ["LISP", "C"]
 # }
 
-	programmer_hash(key) = 
+	programmer_hash = 
  		{
         :grace_hopper => {
           :known_for => "COBOL",
